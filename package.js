45c4fa05faff1800a72a0d272d4876d2dfc960d7
dr.dimitru <dr.dimitru@gmail.com>
2014-11-30 19:56:57 +0300

    Dependency upd

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles(['ostrio:neo4jreactivity_driver.js', 'ostrio:neo4jreactivity_methods.js', 'ostrio:neo4jreactivity_collection.js']);
   api.use('underscore', ['client', 'server']);
-  api.use(['jparker:crypto-sha256@0.1.1', 'ostrio:neo4jdriver@0.1.12'])
+  api.use(['jparker:crypto-sha256@0.1.1', 'ostrio:neo4jdriver@0.1.13'])
 });
 
 Npm.depends({
