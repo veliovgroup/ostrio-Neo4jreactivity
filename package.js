c06460fb4f1db111411a6bb0eb27322a73481ab2
dr.dimitru <dr.dimitru@gmail.com>
2015-01-15 23:10:40 +0300

    Major update
     - Add jshint globals and rules
     - Refactored
     - Better Sensitivities look up
     - Better mapping
     - UPD due to Meteor updates
     - Add on use: sha and npm-container

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:neo4jreactivity',
   summary: 'Meteor.js Neo4j database pseudo-reactivity layer',
-  version: '0.3.6',
+  version: '0.4.0',
   git: 'https://github.com/VeliovGroup/ostrio-Neo4jreactivity.git'
 });
 
@@ -11,7 +11,9 @@ Package.onUse(function(api) {
   api.use('underscore', ['client', 'server']);
   api.use('tracker', 'client');
   api.use('session', 'client');
-  api.use('ostrio:neo4jdriver@0.2.1')
+  api.use('sha', ['client', 'server']);
+  api.use('ostrio:neo4jdriver@0.2.4');
+  api.use('npm-container@1.0.0');
 });
 
 Npm.depends({
