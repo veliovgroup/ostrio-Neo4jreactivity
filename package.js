bd785223733e6a17e2cfcb9b1af7d6738f9702f8
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-08-01 01:20:07 +0300

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:neo4jreactivity',
   summary: 'Meteor.js Neo4j database reactivity layer',
-  version: '0.8.4',
+  version: '0.8.5',
   git: 'https://github.com/VeliovGroup/ostrio-Neo4jreactivity.git'
 });
 
